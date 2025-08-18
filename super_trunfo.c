a06489108901d731533921133056f444780d7cfc
Gabriel-gsd <ggaygher@hotmail.com>
2025-08-18 23:54:57 +0000

    novo commit

---
+++ super_trunfo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+    char estado1[20], estado2[20], codigo1[4], codigo2[4], cidade1[20], cidade2[20];
+    int populacao1, populacao2, pontos1, pontos2;
+    float area1, area2, pib1, pib2;
+
+    printf ("Digite o Estado da 1ª carta: \n")
+    scanf ("%s", &estado1);
+    
+    return 0;
+}
